e6bb7557221107bea1f2c3f6b265d392bf12c44c
marcosschlick <marcosschlick@gmail.com>
2024-12-22 12:08:35 -0300

    create spring boot files

---
+++ src/main/java/br/com/ifsul/contador_presenca/ContadorDePresencaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifsul.contador_presenca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContadorDePresencaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContadorDePresencaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifsul/contador_presenca/ContadorDePresencaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifsul.contador_presenca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContadorDePresencaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
